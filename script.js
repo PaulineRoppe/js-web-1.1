93f1bc2cbd51896cb112ed710246272924851981
Roppe Pauline <roppepauline@gmail.com>
2019-02-07 14:43:45 +0100

    update

--- script.js
+++ script.js
@@ -12,4 +12,20 @@ import "./style.scss";
   Put the JavaScript code you want below.
 */
 
-console.log("Hey look in your browser console. It works!");
+//Afficher un carré noir dans le div #carre
+//document.querySelector("#carre").innerHTML='<div style= "margin:30px; width:100px; height:100px; background-color:black;"></div>';
+document.querySelector("#carre").style.backgroundColor="black";
+
+document.querySelector("#buttonBlue").addEventListener("click",() => {
+   document.querySelector("#carre").style.backgroundColor="blue";
+});
+document.querySelector("#buttonRed").addEventListener("click",() => {
+   document.querySelector("#carre").style.backgroundColor="red";
+});
+document.querySelector("#buttonGreen").addEventListener("click",() => {
+   document.querySelector("#carre").style.backgroundColor="green";
+});
+
+
+//Quand on clique sur un bouton
+//Le carré doit prendre la couleur notée
